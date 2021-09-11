d18cc0ca1f7d26d27da86cff9d51d57971c9064f
waltergarrido <blackwalter7@gmail.com>
2021-09-11 12:45:47 -0300

    schemas

---
+++ backend/src/schemas/schemaOperario.js
@@ -0,0 +1,15 @@
+import { Schema, model } from 'mongoose';
+
+const schemaOperario = new Schema({
+    numeroEmpleado: String,
+    DNI: String,
+    nombre: String,
+    apellido: String,
+    telefono: String,
+    nombreUsuario: String,
+    contraseña: String,
+    email: String,
+    sector: String
+});
+
+export const modelOperario = model('operario', schemaOperario, 'operario');
