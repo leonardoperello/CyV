1c4e8fd56d802801f54b033b627450ea4bc372a8
waltergarrido <blackwalter7@gmail.com>
2021-09-14 09:47:44 -0300

    endpoint oti, rosca y tarea

--- backend/src/controllers/rosca.js
+++ backend/src/controllers/rosca.js
@@ -3,8 +3,60 @@ import { modelRosca } from "../schemas/schemaRosca";
 const router = express.Router();
 
 router.get("/", async function (req, res) {
-  const result = await modelRosca.find({});
-  res.json(result.reverse());
+    try {
+        const result = await modelRosca.find({});
+        res.json(result.reverse());
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+router.get('/:id', async function (req, res) {
+    try {
+        const id = req.params.id;
+        const result = await modelRosca.findOne({ _id: id });
+        res.json(result);
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+
+
+router.post('/', async function (req, res) {
+    let data = req.body;
+    try {
+        const resultInsert = await modelRosca.create(data);
+        res.json(resultInsert);
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+router.patch('/:id', async function (req, res) {
+    try {
+        let tarea = { _id: req.params.id };
+        let data = req.body;
+        const result = await modelRosca.findOneAndUpdate(tarea, data);
+        res.json(result)
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+
+router.delete('/:id', async function (req, res) {
+    try {
+        const id = req.params.id;
+        const result = await modelRosca.deleteOne({ _id: id });
+        res.json(result)
+    } catch (error) {
+        console.log(error);
+    }
+
 });
 
 export default router;
--- backend/src/controllers/tarea.js
+++ backend/src/controllers/tarea.js
@@ -3,10 +3,61 @@ import { modelTarea } from '../schemas/schemaTarea';
 const router = express.Router();
 
 router.get('/', async function (req, res) {
-    const result = await modelTarea.find({});
-    res.json(result.reverse());
+    try {
+        const result = await modelTarea.find({});
+        res.json(result.reverse());
+    }
+    catch (error) {
+        console.log(error);
+    }
 });
 
+
+router.get('/:id', async function (req, res) {
+    try {
+        const id = req.params.id;
+        const result = await modelTarea.findOne({ _id: id });
+        res.json(result);
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+
+router.post('/', async function (req, res) {
+    let data = req.body;
+    try {
+        const resultInsert = await modelTarea.create(data);
+        res.json(resultInsert);
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+router.patch('/:id', async function (req, res) {
+    try {
+        let tarea = { _id: req.params.id };
+        let data = req.body;
+        const result = await modelTarea.findOneAndUpdate(tarea, data);
+        res.json(result)
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+
+router.delete('/:id', async function (req, res) {
+    try {
+        const id = req.params.id;
+        const result = await modelTarea.deleteOne({ _id: id });
+        res.json(result)
+    } catch (error) {
+        console.log(error);
+    }
+
+});
 
 
 export default router;
