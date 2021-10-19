465a16ad9dd791fbf3c069236416b6f96a394649
mariano <marianocampe94@gmail.com>
2021-10-19 08:08:48 -0300

    nuevos cambios

--- backend/src/controllers/altaAsignarTareas.js
+++ backend/src/controllers/altaAsignarTareas.js
@@ -2,14 +2,17 @@ import { modelOperario } from '../schemas/schemaOperario';
 import { modelOti } from '../schemas/schemaOti';
 import { modelTarea } from '../schemas/schemaTarea';
 
-export async function obtenerTareas(id_sector) {
-    const otis = await modelOti.find({ idsector: id_sector });
-    otis = otis?.filter((oti) => oti.estado[oti.estado.length] === "iniciada" || oti.estado[oti.estado.length] === "en progreso");
-    if (otis.length) {
-        const tareas = otis.tareas.filter((tarea) => tarea.estado[tarea.estado.length].tipoDeEstado.nombre === "iniciada" || tarea.estado[tarea.estado.length].tipoDeEstado.nombre === "detenida");
-        return tareas;
+export async function obtenerTareas(data) {
+    const otis = await modelOti.find();
+    const arre = otis.filter((oti) => oti.estados[oti.estados.length - 1]?.tipoEstado?.nombre === "inicializada" || oti.estados[oti.estados.length - 1]?.tipoEstado?.nombre === "en progreso");
+    if (arre.length) {
+        // const arre2;
+        for (let i = 0; i < arre.length; i++) {
+
+            const arre2 = arre.tareas.filter((tarea) => tarea.estado[tarea.estado.length - 1]?.tipoEstado?.nombre === "iniciada" || tarea.estado[tarea.estado.length - 1]?.tipoEstado?.nombre === "detenida");
+            console.log(arre2);
+        }
     }
-    // Que retorno en caso de que oti este vacia?
 }
 
 export async function verificarEstadoTarea(data) {
@@ -18,5 +21,5 @@ export async function verificarEstadoTarea(data) {
     if (tarea.estado[tarea.estado.length].tipoDeEstado.nombre === "iniciada" || tarea.estado[tarea.estado.length].tipoDeEstado.nombre === "detenida") {
         tarea.estado[tarea.estado.length].tipoDeEstado.nombre === "en progreso";
     }
-    // es necesario retornar algo aca?
 }
+
--- backend/src/routers/tarea.js
+++ backend/src/routers/tarea.js
@@ -4,52 +4,56 @@ import { modelTarea } from "../schemas/schemaTarea";
 const router = new express.Router();
 
 router.get("/", async function (req, res) {
-  try {
-    const result = await modelTarea.find({});
-    res.json(result);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const result = await modelTarea.find({});
+        res.json(result);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 router.post("/", async function (req, res) {
-  try {
-    let data = req.body;
-    const resultado = await altaTarea(data);
-    res.status(200).send(resultado);
-  } catch (error) {
-    res.status(400).send(error);
-  }
+    try {
+        let data = req.body;
+        const resultado = await altaTarea(data);
+        res.status(200).send(resultado);
+    } catch (error) {
+        res.status(400).send(error);
+    }
 });
 router.get("/:id", async function (req, res) {
-  try {
-    const id = req.params.id;
-    const resultado = await modelTarea.findOne({ _id: id });
-    res.status(200).send(resultado);
-  } catch (error) {
-    res.status(400).send(error);
-  }
+    try {
+        const id = req.params.id;
+        const resultado = await modelTarea.findOne({ _id: id });
+        res.status(200).send(resultado);
+    } catch (error) {
+        res.status(400).send(error);
+    }
 });
 
 router.patch("/:id", async function (req, res) {
-  try {
-    let tarea = { _id: req.params.id };
-    let data = req.body;
-    const resultado = await modelTarea.findOneAndUpdate(tarea, data);
-    res.status(200).send(resultado);
-  } catch (error) {
-    res.status(400).send(error);
-  }
+    try {
+        let tarea = { _id: req.params.id };
+        let data = req.body;
+        const resultado = await modelTarea.findOneAndUpdate(tarea, data);
+        res.status(200).send(resultado);
+    } catch (error) {
+        res.status(400).send(error);
+    }
 });
 
 router.delete("/:id", async function (req, res) {
-  try {
-    const id = req.params.id;
-    const resultado = await modelTarea.deleteOne({ _id: id });
-    res.status(200).send(resultado);
-  } catch (error) {
-    res.status(400).send(error);
-  }
+    try {
+        const id = req.params.id;
+        const resultado = await modelTarea.deleteOne({ _id: id });
+        res.status(200).send(resultado);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.get("/asignar/:id", async (req, res, next) => {
+    await obtenerTareas(req.params.id);
 });
 
 export default router;
