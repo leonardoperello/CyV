516b9f913dd3b938af14c9805466442fc01d8640
waltergarrido <blackwalter7@gmail.com>
2021-11-06 16:36:13 -0300

    agrego fecha fin a estado antes de cambiarlo

--- backend/src/controllers/altaEstado.controllers.js
+++ backend/src/controllers/altaEstado.controllers.js
@@ -3,8 +3,8 @@ import moment from "moment";
 
 export async function cargarEstado(data) {
   const estado = {
-    fechaInicio: moment().format(data.fechaInicio),
-    fechaFin: data.fechaFin ? moment().format(data.fechaFin) : null,
+    fechaInicio: moment().format("YYYY/MM/DD"),
+    fechaFin: null, // cuando es un estado nuevo fecha fin siempre es null xq no sabemos cuando termina, cuando hay cambios de estado ahi se carga este campo antes de generar uno nuevo
     observacion: data.observacion ? data.observacion : '',
     tipoEstado: data.tipoEstado,
   };
