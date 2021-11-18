0c5310d15ad826e318d08071ab6a45cc2a6a67ee
waltergarrido <blackwalter7@gmail.com>
2021-11-18 11:29:33 -0300

    query por params

--- backend/src/controllers/altaAsignarTareas.js
+++ backend/src/controllers/altaAsignarTareas.js
@@ -4,80 +4,82 @@ import { modelTarea } from "../schemas/schemaTarea";
 import { cargarEstado } from "../controllers/altaEstado.controllers";
 
 export async function obtenerOtisDelSector(nombreSector) {
-  const querySector = { nombre: nombreSector };
-  const sector = await modelSector.findOne(querySector);
-  const otis = await modelOti.find({}); // si los estados estan vacios este metodo pincha
-  const otisFiltradas = otis.filter((oti) =>
-    oti.sector.find(
-      (sec) =>
-        sec.nombre === sector.nombre &&
-        sec.activo &&
-        (oti.estados[oti.estados?.length - 1].tipoEstado?.nombre ===
-          "en progreso" ||
-          oti.estados[oti.estados?.length - 1].tipoEstado?.nombre ===
-          "detenida" ||
-          oti.estados[oti.estados?.length - 1].tipoEstado?.nombre ===
-          "iniciada")
-    )
-  ); //me quedo con las otis que se encuentran en el sector listas para trabajar.
-  if (otisFiltradas.length) {
-    const otis = otisFiltradas.map((oti) => {
-      return {
-        idOti: oti._id,
-        fechaInicio: oti.fechaInicio,
-        fechaFin: oti.fechaFin,
-        rosca: oti.rosca,
-      };
-    });
-    return otis;
-  }
+    const querySector = { nombre: nombreSector };
+    const sector = await modelSector.findOne(querySector);
+    const otis = await modelOti.find({}); // si los estados estan vacios este metodo pincha
+    const otisFiltradas = otis.filter((oti) =>
+        oti.sector.find(
+            (sec) =>
+                sec.nombre === sector.nombre &&
+                sec.activo &&
+                (oti.estados[oti.estados?.length - 1].tipoEstado?.nombre ===
+                    "en progreso" ||
+                    oti.estados[oti.estados?.length - 1].tipoEstado?.nombre ===
+                    "detenida" ||
+                    oti.estados[oti.estados?.length - 1].tipoEstado?.nombre ===
+                    "iniciada")
+        )
+    ); //me quedo con las otis que se encuentran en el sector listas para trabajar.
+    if (otisFiltradas.length) {
+        const otis = otisFiltradas.map((oti) => {
+            return {
+                idOti: oti._id,
+                fechaInicio: oti.fechaInicio,
+                fechaFin: oti.fechaFin,
+                rosca: oti.rosca,
+            };
+        });
+        return otis;
+    }
 }
 
 export async function obtenerTareas(idOti) {
-  const queryOti = { _id: idOti };
-  const oti = await modelOti.findOne(queryOti);
+    const queryOti = { _id: idOti };
+    const oti = await modelOti.findOne(queryOti);
 
-  const tareasFiltradas = oti.tareas.filter(
-    (tarea) =>
-      tarea.estado[tarea.estado.length - 1]?.tipoEstado?.nombre ===
-      "iniciada" ||
-      tarea.estado[tarea.estado.length - 1]?.tipoEstado?.nombre === "detenida"
-  );
+    const tareasFiltradas = oti.tareas.filter(
+        (tarea) =>
+            tarea.estado[tarea.estado.length - 1]?.tipoEstado?.nombre ===
+            "iniciada" ||
+            tarea.estado[tarea.estado.length - 1]?.tipoEstado?.nombre === "detenida"
+    );
 
-  return tareasFiltradas;
+    return tareasFiltradas;
 }
 
 export async function asignarTareaOperario(data) {
-  const queryOti = { _id: data.idOti };
-  const idOperario = data.idOperario;
-  const tareas = data.tareas;
-  const oti = await modelOti.findOne(queryOti);
-  const estado = "";
-  data["tipoEstado"] = {
-    nombre: "iniciada",
-    descripcion: "",
-  };
-  tareas.forEach(async (tarea) => {
-    tarea.idOperario = idOperario; //si la tarea tenia un un operario asignado xq estaba en detenida lo piso
-    if (
-      tarea.estado[tarea.estado.length - 1]?.tipoEstado?.nombre === "detenida"
-    ) {
-      estado = await cargarEstado(data); //si esta en iniciada queda asi , el operario la cambia a en progreso
-      tarea.estado.push(estado);
-    }
-    await oti.tareas.find((tareaOti) => {
-      // busco en la oti la tarea y la actualizo
-      if (tareaOti._id.equals(tarea._id)) {
-        tareaOti.idOperario = tarea.idOperario; // guardo el nuevo estado en tarea
-        if (estado !== "") {
-          tareaOti.estado.push(estado);
+
+    const queryOti = { _id: data.idOti };
+    const idOperario = data.idOperario;
+    const tareas = data.tareas;
+    const oti = await modelOti.findOne(queryOti);
+    const estado = "";
+    data["tipoEstado"] = {
+        nombre: "iniciada",
+        descripcion: "",
+    };
+
+    tareas.forEach(async (tarea) => {
+        tarea.idOperario = idOperario; //si la tarea tenia un un operario asignado xq estaba en detenida lo piso
+        if (
+            tarea.estado[tarea.estado.length - 1]?.tipoEstado?.nombre === "detenida"
+        ) {
+            estado = await cargarEstado(data); //si esta en iniciada queda asi , el operario la cambia a en progreso
+            tarea.estado.push(estado);
         }
-      }
+        await oti.tareas.find((tareaOti) => {
+            // busco en la oti la tarea y la actualizo
+            if (tareaOti._id.equals(tarea._id)) {
+                tareaOti.idOperario = tarea.idOperario; // guardo el nuevo estado en tarea
+                if (estado !== "") {
+                    tareaOti.estado.push(estado);
+                }
+            }
+        });
+        await modelTarea.findOneAndUpdate({ _id: tarea._id }, tarea);
     });
-    await modelTarea.findOneAndUpdate({ _id: tarea._id }, tarea);
-  });
 
-  //buscao oti y actualizo
+    //buscao oti y actualizo
 
-  return await modelOti.findOneAndUpdate(queryOti, oti);
+    return await modelOti.findOneAndUpdate(queryOti, oti);
 }
--- backend/src/routers/tarea.js
+++ backend/src/routers/tarea.js
@@ -22,12 +22,12 @@ router.get("/obtenerOtis/:nombre", async function (req, res) {
   }
 });
 
-router.get("/obtenerTareas", async function (req, res) {
+router.get("/obtenerTareas/:id", async function (req, res) {
   try {
-    if (typeof req.query.id !== "string" || req.query.id.length !== 24) {
+    if (typeof req.params.id !== "string" || req.params.id.length !== 24) {
       return res.status(400).send("El formato del id es incorrecto");
     }
-    const idOti = req.query.id;
+    const idOti = req.params.id;
     const result = await obtenerTareas(idOti);
     res.json(result);
   } catch (error) {
@@ -52,34 +52,6 @@ router.post("/asignarTarea", async function (req, res) {
         .send("El formato del id del operario es incorrecto");
     }
     if (Object.keys(req.body.tareas).length === 0) {
-      return res.status(400).send("No existe una tarea");
-    }
-    const resultado = await asignarTareaOperario(data);
-    res.status(200).send(resultado);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-  try {
-    let data = req.body;
-    if (typeof req.body.observacion !== "string") {
-      console.log("OBSERVACION");
-      return res.status(400).send("El formato de la observacion es incorrecto");
-    }
-    if (typeof req.body.idOti !== "string" || req.body.idOti.length !== 24) {
-      console.log("OTI");
-      return res.status(400).send("El formato del id de Oti es incorrecto");
-    }
-    if (
-      typeof req.body.idOperario !== "string" ||
-      req.body.idOperario.length !== 24
-    ) {
-      console.log("OPERARIO");
-      return res
-        .status(400)
-        .send("El formato del id del operario es incorrecto");
-    }
-    if (Object.keys(req.body.tareas).length === 0) {
-      console.log("tarea");
       return res.status(400).send("No existe una tarea");
     }
     const resultado = await asignarTareaOperario(data);
